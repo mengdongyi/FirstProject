29905fd000178ca688a578cbc9b6f912e3552353
mengdongyi <mdy41034264@qq.com>
2016-07-31 03:19:57 +0800

    add

--- CSharp/ConsoleApp/ConsoleApp/Program.cs
+++ CSharp/ConsoleApp/ConsoleApp/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!!!");
+            }
         }
     }
 }
